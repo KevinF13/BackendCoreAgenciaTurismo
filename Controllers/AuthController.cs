f66b2d76f59f7bc1ef599c6db3bc085b3d57acd3
KevinF13 <kevinfajardo110@gmail.com>
2023-06-19 16:19:21 -0500

    Solucion de problema del post status 500

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using BackEndCoreAgencia.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackEndCoreAgencia.Controllers
+{
+    public class AuthController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Auth/login")]
+        public IHttpActionResult AutenticarUsuario(Cliente cliente)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT COUNT(*) FROM Cliente WHERE email = @Usuario AND contrasena = @Password";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.Add("@Usuario", SqlDbType.NVarChar).Value = cliente.email;
+                        command.Parameters.Add("@Password", SqlDbType.NVarChar).Value = cliente.contrasena;
+
+                        object result = command.ExecuteScalar();
+
+                        if (result != null && int.TryParse(result.ToString(), out int count) && count > 0)
+                        {
+                            return Ok(new { message = "Autenticación exitosa" });
+                        }
+                    }
+                }
+
+                return BadRequest("Usuario o contraseña incorrectos");
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Cors;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.Http;
 
 namespace BackEndCoreAgencia.Controllers
@@ -170,30 +173,68 @@ namespace BackEndCoreAgencia.Controllers
 
         //-------------------------------------------------------------------//
         //CREACION DE LOGIN DE CLIENTES
-        [HttpPost]
-        [Route("api/Cliente/login")]
-        public bool AuthenticateUser(Cliente cliente)
-        {
-            string strConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
-            using (SqlConnection connection = new SqlConnection(strConn))
-            {
-                connection.Open();
+        //[HttpPost]
+        //[Route("api/Cliente/login")]
+        //public bool AuthenticateUser(Cliente cliente)
+        //{
+        //    string strConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+        //    using (SqlConnection connection = new SqlConnection(strConn))
+        //    {
+        //        connection.Open();
 
-                string query = "SELECT COUNT(*) FROM Cliente WHERE email = @Email AND contrasena = @Contrasena";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@Email", cliente.email);
-                    command.Parameters.AddWithValue("@Contrasena", cliente.contrasena);
+        //        Console.WriteLine(cliente.email);
+        //        Console.WriteLine(cliente.contrasena);
 
-                    int count = Convert.ToInt32(command.ExecuteScalar());
-                    return count > 0;
-                }
-            }
-        }
+        //        string query = "SELECT COUNT(*) FROM Cliente WHERE email =  @Email AND contrasena = '123'";
+        //        using (SqlCommand command = new SqlCommand(query, connection))
+        //        {
+        //            command.Parameters.AddWithValue("@Email", cliente.email);
+        //            command.Parameters.AddWithValue("@Contrasena", cliente.contrasena);
+
+        //            Console.WriteLine("@Email: " + cliente.email);
+        //            Console.WriteLine("@Contraseña: " + cliente.contrasena);
+
+        //            int count = Convert.ToInt32(command.ExecuteScalar());
+        //            return count > 0;
+        //        }
+        //    }
+        //}
+        //[HttpPost]
+        //[Route("api/Cliente/login")]
+        //public IHttpActionResult AutenticarUsuario(Cliente cliente)
+        //{
+        //    string connectionString = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+        //    try
+        //    {
+        //        using (SqlConnection connection = new SqlConnection(connectionString))
+        //        {
+        //            connection.Open();
+
+        //            string query = "SELECT COUNT(*) FROM Cliente WHERE email = @Usuario AND contrasena = @Password";
+        //            using (SqlCommand command = new SqlCommand(query, connection))
+        //            {
+        //                command.Parameters.Add("@Usuario", SqlDbType.NVarChar).Value = cliente.email;
+        //                command.Parameters.Add("@Password", SqlDbType.NVarChar).Value = cliente.contrasena;
+
+        //                object result = command.ExecuteScalar();
+
+        //                if (result != null && int.TryParse(result.ToString(), out int count) && count > 0)
+        //                {
+        //                    return Ok(new { message = "Autenticación exitosa" });
+        //                }
+        //            }
+        //        }
+
+        //        return BadRequest("Usuario o contraseña incorrectos");
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return InternalServerError();
+        //    }
+        //}
 
     }
-
-
-
 }
+
+
 
